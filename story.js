e822a0902b9308f5cf60dae4db7b51c3f2f582cc
Windlordo <marc_crombie@hotmail.com>
2025-04-10 11:06:41 +0100

    Update story.js

--- story.js
+++ story.js
@@ -1,1 +1,70 @@
+const textElement = document.getElementById('text')
+const optionButtonsElement = document.getElementById('option-buttons')
 
+let state = {}
+
+function startGame() {
+  state = {}
+  showTextNode(1)
+}
+
+function showTextNode(textNodeIndex) {
+  const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+  textElement.innerText = textNode.text
+  while (optionButtonsElement.firstChild) {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+  }
+
+  textNode.options.forEach(option => {
+    if (showOption(option)) {
+      const button = document.createElement('button')
+      button.innerText = option.text
+      button.classList.add('btn')
+      button.addEventListener('click', () => selectOption(option))
+      optionButtonsElement.appendChild(button)
+    }
+  })
+}
+
+function showOption(option) {
+  return option.requiredState == null || option.requiredState(state)
+}
+
+function selectOption(option) {
+  const nextTextNodeId = option.nextText
+  if (nextTextNodeId <= 0) {
+    return startGame()
+  }
+  state = Object.assign(state, option.setState)
+  showTextNode(nextTextNodeId)
+}
+
+const textNodes = [
+  {
+    id: 1,
+    text: "You open your eyes and notice you're in a empty white room with a pipe and button lying on the floor.",
+    options: [
+      {
+        text: 'Pick up the pipe',
+        setState: { pipe: true },
+        nextText: 3
+      },
+      {
+        text: 'Push the button.',
+        nextText: 2
+      }
+    ]
+  },
+  {
+    id: 2,
+    text: "You hear a loud noise followed by everything going dark. You are dead.",
+    options: [
+      {
+        text: "Restart",
+        nextText:-1
+      }
+    ]
+  },
+        
+
+startGame()
