6c94a466e9570da0cb112cc5000b8c4c57422263
Windlordo <marc_crombie@hotmail.com>
2025-05-04 20:35:41 +0100

    Update story.js

--- story.js
+++ story.js
@@ -39,7 +39,7 @@ function selectOption(option) {
   showTextNode(nextTextNodeId)
 }
 
-const textNode = [
+const textNodes = [
   {
     id: 1,
     text: "You open your eyes and notice you're in a empty white room with a pipe and button lying on the floor.",
